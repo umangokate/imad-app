57685cba8113e2febdf9156ac9c4ef88f47fb3e3
umangokate <umangokate@gmail.com>
2017-09-14 16:54:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,62 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+/*var articles = {
+	'article-one':{
+		title:'Article One',
+		heading:'Article One',
+		date:'12/6/17',
+		content:'<p>My first article<p>'
+	}
+
+};
+
+
+function createTemplate(data)
+{
+	var title = data.title;
+	//console.log(title);
+	var heading = data.heading;
+	var date = data.date;
+	var content = data.content;
+
+	var htmlTemplate =
+	`<html>
+		<head>
+			<title>${title}</title>
+			<link href="/ui/style.css" rel="stylesheet" />
+		</head>
+		<body>
+			<div id="container">
+				<div>
+					<h3>${heading}</h3>
+				</div>
+				<hr>
+				<div>
+					${date}
+				</div>
+				<hr>
+				<div>
+					${content}
+				</div>
+				<hr>
+			<div>
+		</body>
+	</html>`;
+
+	return htmlTemplate;
+}*/
+var counter = 0;
+app.get('/ui/main.js',function(req, res){
+	res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/counter',function(req,res){
+	counter = counter+1;
+
+	res.send(counter.toString());
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -13,15 +69,23 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+/*app.get('/:articleName', function(req,res){
+	var articleName = req.params.articleName;
+	res.send(createTemplate(articles[articleName]));
+});*/
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names = [];
+app.get('/submit-name',function(req,res){
+	var name = req.query.name;
+	names.push(name);
+	res.send(JSON.stringify(names));
+});
 
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
